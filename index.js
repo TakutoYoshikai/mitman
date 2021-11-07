8d3042d0568a3850af62ad6b5e3a2173fef42d0e
lucky <takuto.yoshikai@gmail.com>
2021-11-07 12:19:52 +0900

    index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+
+const name = process.argv.slice(4).reduce((a, b) => a + " " + b);
+
+fs.writeFileSync("./README.md", "# " + process.argv[3] + "\n\n### Requirements\n\n### Usage\n**install**\n\n\n### License\nMIT License");
+
+fs.writeFileSync("./LICENSE.txt", "Copyright " + (new Date()).getFullYear() + " " + name + "\n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.");
